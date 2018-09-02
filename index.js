3b5b106a738eccf045fcec535227f6e1e2b48a34
Shih-Kai Lin <loveskai1988@gmail.com>
2018-09-02 16:54:54 -0700

    retrieved flight list

---
+++ index.js
@@ -0,0 +1,40 @@
+const puppeteer = require('puppeteer');
+
+(async () => {
+  const browser = await puppeteer.launch({headless: false});
+  const page = await browser.newPage();
+
+  const host = 'https://www.expedia.com/Flights-Search';
+  const startDate = '10/02/2018';
+  const endDate = '10/11/2018';
+  const cabinClass = 'business';
+  const pageUrl =
+    host + '?flight-type=on&starDate=' + startDate + '&endDate=' + endDate + '&mode=search&trip=roundtrip&' +
+    'leg1=from:Los+Angeles,+CA+(LAX-Los+Angeles+Intl.),to:Taipei,+Taiwan+(TPE-All+Airports),departure:' + startDate +
+    'TANYT&leg2=from:Taipei,+Taiwan+(TPE-All+Airports),to:Los+Angeles,+CA+(LAX-Los+Angeles+Intl.),departure:' +
+    endDate + 'TANYT&passengers=children:0,adults:1,seniors:0,infantinlap:Y&options=cabinclass:' + cabinClass + ',';
+
+  // console.log('go to: ' + pageUrl);
+  await page.goto(pageUrl);
+
+  const flightList = await page.evaluate(() => {
+    const list = Array.from(document.getElementById('flightModuleList').querySelectorAll('li.offer-listing'));
+    return list.map(item => {
+      const airline = item.querySelector("span[data-test-id='airline-name']");
+      const price = item.querySelector("span[data-test-id='listing-price-dollars']");
+      return {
+        airline: airline ? airline.innerText.trim() : null,
+        price: price ? price.innerText.trim() : null
+      };
+    });
+  });
+
+  const processedFlightList = Array.from(flightList).filter(f => f.price).map(
+    f => Object.assign({}, f, {
+      price: parseFloat(f.price.replace(/[^0-9.-]+/g, ''))
+    })
+  );
+  console.log(JSON.stringify(processedFlightList));
+
+  await browser.close();
+})();
